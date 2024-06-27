13ba6852ee2678e7295f000dfcf862a46e89505e
jorge paredes <narutoninja367@gmail.com>
2024-06-27 17:53:25 -0500

    5.0

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -360,9 +360,9 @@ Partial Class Form1
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(593, 634)
+        Me.Button3.Location = New System.Drawing.Point(404, 624)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.Size = New System.Drawing.Size(75, 33)
         Me.Button3.TabIndex = 18
         Me.Button3.Text = "Button3"
         Me.Button3.UseVisualStyleBackColor = True
